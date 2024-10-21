3879d105ec8bd7a83ce0c2975dec3a3672696f6a
abdera2004 <ruanpereira405@gmail.com>
2024-10-20 22:58:18 -0300

    arrumando css

--- components/Navbar/index.php
+++ components/Navbar/index.php
@@ -1,6 +1,52 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
+
+    <!-- Barra de Navegação -->
+    <div class="corpo col-2 d-flex flex-column align-items-start position-relative" style="height: 100vh; overflow: hidden;">
+        <!-- Logo Section -->
+        <div class="logo d-flex flex-column w-100 align-items-center justify-content-center mb-4 position-relative">
+            <a href="../Home/index.php">
+                <i class="bi bi-shop" style="font-size: 100px;"></i>
+            </a>
+        </div>
+
+        <!-- Menu Items -->
+        <div class="d-flex flex-column align-items-center w-100 ms-5">
+            <div class="tamanho-menu d-flex flex-row align-items-center justify-content-start w-100">
+                <a href="../Cliente/index.php" class="icone-e-texto">
+                    <i class="bi bi-person-fill"></i>
+                    <b class="ps-2 texto-menu">Clientes</b>
+                </a>
+            </div>
+            <div class="tamanho-menu d-flex flex-row align-items-center justify-content-start w-100">
+                <a href="../Produtos/index.php" class="icone-e-texto">
+                    <i class="bi bi-archive-fill"></i>
+                    <b class="ps-2 texto-menu">Produtos</b>
+                </a>
+            </div>
+            <div class="tamanho-menu d-flex flex-row align-items-center justify-content-start w-100">
+                <a href="../Pedidos/index.php" class="icone-e-texto">
+                    <i class="bi bi-folder-symlink-fill"></i>
+                    <b class="ps-2 texto-menu">Pedidos</b>
+                </a>
+            </div>
+        </div>
+
+        <!-- Dropdown Section -->
+        <div class="mt-auto w-100 d-flex justify-content-center">
+            <div class="dropdown-center">
+                <button class="dropdown-toggle bg-transparent border-0" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="bi bi-person-circle" style="font-size: 100px;"></i>
+                </button>
+                <ul class="dropdown-menu border border-black">
+                    <li><a class="dropdown-item d-flex justify-content-center fw-bold border-bottom border-1 border-black" href="../Adm/index.php">Perfil</a></li>
+                    <li><a class="dropdown-item fw-bold d-flex justify-content-center text-danger" href="../Login/index.php">Fazer logoff</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
     <style>
         body, html {
             margin: 0;
@@ -38,11 +84,6 @@
             height: 100px;
             width: 300px;
             color: white;
-        }
-
-        a {
-            all: unset; /* Remove underline from links */
-            cursor: pointer;
         }
 
         .dropdown-toggle::after {
@@ -120,47 +161,3 @@
         }
 
     </style>
-    <!-- Barra de Navegação -->
-    <div class="corpo col-2 d-flex flex-column align-items-start position-relative" style="height: 100vh; overflow: hidden;">
-        <!-- Logo Section -->
-        <div class="logo d-flex flex-column w-100 align-items-center justify-content-center mb-4 position-relative">
-            <a href="../Home/index.php">
-                <i class="bi bi-shop" style="font-size: 100px;"></i>
-            </a>
-        </div>
-
-        <!-- Menu Items -->
-        <div class="d-flex flex-column align-items-center w-100 ms-5">
-            <div class="tamanho-menu d-flex flex-row align-items-center justify-content-start w-100">
-                <a href="../Cliente/index.php" class="icone-e-texto">
-                    <i class="bi bi-person-fill"></i>
-                    <b class="ps-2 texto-menu">Clientes</b>
-                </a>
-            </div>
-            <div class="tamanho-menu d-flex flex-row align-items-center justify-content-start w-100">
-                <a href="../Produtos/index.php" class="icone-e-texto">
-                    <i class="bi bi-archive-fill"></i>
-                    <b class="ps-2 texto-menu">Produtos</b>
-                </a>
-            </div>
-            <div class="tamanho-menu d-flex flex-row align-items-center justify-content-start w-100">
-                <a href="../Pedidos/index.php" class="icone-e-texto">
-                    <i class="bi bi-folder-symlink-fill"></i>
-                    <b class="ps-2 texto-menu">Pedidos</b>
-                </a>
-            </div>
-        </div>
-
-        <!-- Dropdown Section -->
-        <div class="mt-auto w-100 d-flex justify-content-center">
-            <div class="dropdown">
-                <button class="dropdown-toggle bg-transparent border-0" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <i class="bi bi-person-circle" style="font-size: 100px;"></i>
-                </button>
-                <ul class="dropdown-menu rounded border border-black">
-                    <li><a class="dropdown-item fw-bold" href="../Adm/index.php">Perfil</a></li>
-                    <li><a class="dropdown-item fw-bold" href="../Login/index.php">Fazer logoff</a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
